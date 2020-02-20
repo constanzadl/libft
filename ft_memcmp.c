585edc22cd387b73223f6dd30cf9067a48517313
Constanza Duarte ledon <cduarte-@e1z3r10p3.42.us.org>
2020-02-20 09:48:58 -0800

    memcmp2

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cduarte- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/20 09:46:13 by cduarte-          #+#    #+#             */
+/*   Updated: 2020/02/20 09:47:30 by cduarte-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
-{	
-	size_t i;
-	unsigned char *s1_ptr;
-	unsigned char *s2_ptr;
+{
+	size_t			i;
+	unsigned char	*s1_ptr;
+	unsigned char	*s2_ptr;
 
 	i = 0;
 	s1_ptr = (unsigned char *)s1;
 	s2_ptr = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
-	while(s1_ptr[i] == s2_ptr[i] &&  n > i)
+	while (s1_ptr[i] == s2_ptr[i] && n > i)
 	{
 		i++;
 	}
-	return(s1_ptr[i] - s2_ptr[i]);
+	return (s1_ptr[i] - s2_ptr[i]);
 }
