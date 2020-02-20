3c7a9d16c264fab30a57e773ce021bdf40681980
Constanza Duarte ledon <cduarte-@e1z3r10p3.42.us.org>
2020-02-20 09:54:11 -0800

    memcmp test

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: cduarte- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 09:46:13 by cduarte-          #+#    #+#             */
-/*   Updated: 2020/02/20 09:47:30 by cduarte-         ###   ########.fr       */
+/*   Updated: 2020/02/20 09:53:30 by cduarte-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,14 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	s1_ptr = (unsigned char *)s1;
 	s2_ptr = (unsigned char *)s2;
-	if (n == 0)
+	if (!n)
 		return (0);
-	while (s1_ptr[i] == s2_ptr[i] && n > i)
+	while (s1_ptr[i] == s2_ptr[i] && n)
 	{
 		i++;
+		n--;
 	}
+	if (n == 0)
+		return (0);
 	return (s1_ptr[i] - s2_ptr[i]);
 }
