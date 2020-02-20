fc91a4f33720e5c93f940109086c1233a0dec9a2
Constanza Duarte ledon <cduarte-@e1z3r10p3.42.us.org>
2020-02-20 09:43:43 -0800

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,16 +3,17 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {	
 	size_t i;
+	unsigned char *s1_ptr;
+	unsigned char *s2_ptr;
 
 	i = 0;
-	while(((unsigned char*)s1)[i] == ((unsigned char*)s2)[i] || n > 0)
-	{
-		i++;
-		n--;
-		if((!((unsigned char *)s1)[i] && !((unsigned char *)s2)[i]) || n == 0)
-			return (0);
-	}
+	s1_ptr = (unsigned char *)s1;
+	s2_ptr = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
-	return(((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+	while(s1_ptr[i] == s2_ptr[i] &&  n > i)
+	{
+		i++;
+	}
+	return(s1_ptr[i] - s2_ptr[i]);
 }
